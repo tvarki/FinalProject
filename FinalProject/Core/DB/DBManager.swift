dd61db9a03a2d03f15bd5353d98e9e9ca7125800
tvarki <peremennik@rambler.ru>
2020-03-04 16:38:33 +0300

    db migration update

--- FinalProject/Core/DB/DBManager.swift
+++ FinalProject/Core/DB/DBManager.swift
@@ -20,44 +20,44 @@ class DBManager {
     var notificationToken: NotificationToken?
     private let condition = NSCondition()
     private let lck = NSLock()
-
+    
     init() {
         setupRealm()
     }
-
+    
     // MARK: - get Favorite
-
+    
     func getFavoriteDataFromDB<P>(type: P.Type) -> Results<P>? where P: MyDBObject {
         guard let database = try? Realm() else { return nil }
         let results = database.objects(type).filter("isFavorite == true").sorted(byKeyPath: "name", ascending: true)
         return results
     }
-
+    
     // MARK: - Realm SetuP
-
+    
     func setupRealm() {
         let config = Realm.Configuration(
-            schemaVersion: 1,
-
+            schemaVersion: 2,
+            
             migrationBlock: { _, oldSchemaVersion in
-                if oldSchemaVersion < 1 {}
-            }
+                if oldSchemaVersion < 2 {
+                    self.deleteAllDatabase()
+                }
+        }
         )
-        // Для всех рилмов указываем глобальную конфигурацию
         Realm.Configuration.defaultConfiguration = config
-
+        
         database = try? Realm()
     }
-
+    
     func getDataFromDB<P: Object>(type: P.Type) -> Results<P>? {
         lck.lock()
         guard let database = try? Realm() else { return nil }
         let results: Results<P> = database.objects(type).sorted(byKeyPath: "name", ascending: true)
-        //         let results: Results<P> = database.objects(type)
         lck.unlock()
         return results
     }
-
+    
     func addData<P: MyDBObject>(object: [P]) {
         lck.lock()
         DispatchQueue(label: "realmBackground").async {
@@ -77,7 +77,7 @@ class DBManager {
             self.lck.unlock()
         }
     }
-
+    
     func getItem<P: Object>(index: String, type: P.Type) -> P? {
         guard let database = try? Realm() else { return nil }
         lck.lock()
@@ -85,7 +85,7 @@ class DBManager {
         lck.unlock()
         return tmp
     }
-
+    
     func changeData<P: MyDBObject>(object: P, type: P.Type) {
         guard let database = try? Realm() else { return }
         let item = database.objects(type).filter("name = %@", object.name).first
@@ -96,56 +96,23 @@ class DBManager {
             }
         }
     }
-
+    
     // MARK: - Delete All Data Base
-
+    
     func deleteAllDatabase() {
         guard let database = try? Realm() else { return }
         try? database.write {
             database.deleteAll()
         }
     }
-
+    
     func deleteFromDb<P: Object>(object: P) {
         guard let database = try? Realm() else { return }
         try? database.write {
             database.delete(object)
         }
     }
-
-//    func subscribe<P:Object>(tableView : UITableView?, type: P.Type ) {
-//        guard let database = try? Realm() else { return }
-//        let results = database.objects(type)
-//
-//        notificationToken = results.observe { [weak self] (changes: RealmCollectionChange) in
-//            guard let tableView = tableView else { return }
-//            switch changes {
-//            case .initial:
-//                tableView.reloadData()
-//            case .update(_, let deletions, let insertions, let modifications):
-//                tableView.beginUpdates()
-//                tableView.deleteRows(
-//                    at: deletions.map({ IndexPath(row: $0, section: 0)}),
-//                    with: .automatic
-//                )
-//                tableView.insertRows(
-//                    at: insertions.map({ IndexPath(row: $0, section: 0) }),
-//                    with: .automatic
-//                )
-//                tableView.reloadRows(
-//                    at: modifications.map({ IndexPath(row: $0, section: 0) }),
-//                    with: .automatic
-//                )
-//                tableView.endUpdates()
-//                //                tableView.reloadData()
-//
-//            case .error(let error):
-//                /// Ошбика может возникнуть только если Realm был создан на бэкграунд очереди
-//                fatalError("\(error)")
-//            }
-//        }
-//    }
-
+    
     func getCount<P: Object>(type: P.Type) -> Int {
         guard let database = try? Realm() else { return 0 }
         lck.lock()
@@ -154,8 +121,3 @@ class DBManager {
         return items.count
     }
 }
-
-// protocol MyObject{
-//    dynamic var _id: String { get set }
-//    dynamic var mainLabel: String { get set }
-// }
