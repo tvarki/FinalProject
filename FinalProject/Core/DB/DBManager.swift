02a9a04910e2f2c930980c496366a25e9cef156e
tvarki <peremennik@rambler.ru>
2020-03-04 18:32:50 +0300

    db update

--- FinalProject/Core/DB/DBManager.swift
+++ FinalProject/Core/DB/DBManager.swift
@@ -20,36 +20,34 @@ class DBManager {
     var notificationToken: NotificationToken?
     private let condition = NSCondition()
     private let lck = NSLock()
-    
+
     init() {
         setupRealm()
     }
-    
+
     // MARK: - get Favorite
-    
+
     func getFavoriteDataFromDB<P>(type: P.Type) -> Results<P>? where P: MyDBObject {
         guard let database = try? Realm() else { return nil }
         let results = database.objects(type).filter("isFavorite == true").sorted(byKeyPath: "name", ascending: true)
         return results
     }
-    
+
     // MARK: - Realm SetuP
-    
+
     func setupRealm() {
         let config = Realm.Configuration(
             schemaVersion: 2,
-            
+
             migrationBlock: { _, oldSchemaVersion in
-                if oldSchemaVersion < 2 {
-                    self.deleteAllDatabase()
-                }
-        }
+                if oldSchemaVersion < 2 {}
+            }
         )
         Realm.Configuration.defaultConfiguration = config
-        
+
         database = try? Realm()
     }
-    
+
     func getDataFromDB<P: Object>(type: P.Type) -> Results<P>? {
         lck.lock()
         guard let database = try? Realm() else { return nil }
@@ -57,7 +55,7 @@ class DBManager {
         lck.unlock()
         return results
     }
-    
+
     func addData<P: MyDBObject>(object: [P]) {
         lck.lock()
         DispatchQueue(label: "realmBackground").async {
@@ -77,7 +75,7 @@ class DBManager {
             self.lck.unlock()
         }
     }
-    
+
     func getItem<P: Object>(index: String, type: P.Type) -> P? {
         guard let database = try? Realm() else { return nil }
         lck.lock()
@@ -85,7 +83,7 @@ class DBManager {
         lck.unlock()
         return tmp
     }
-    
+
     func changeData<P: MyDBObject>(object: P, type: P.Type) {
         guard let database = try? Realm() else { return }
         let item = database.objects(type).filter("name = %@", object.name).first
@@ -96,23 +94,23 @@ class DBManager {
             }
         }
     }
-    
+
     // MARK: - Delete All Data Base
-    
+
     func deleteAllDatabase() {
         guard let database = try? Realm() else { return }
         try? database.write {
             database.deleteAll()
         }
     }
-    
+
     func deleteFromDb<P: Object>(object: P) {
         guard let database = try? Realm() else { return }
         try? database.write {
             database.delete(object)
         }
     }
-    
+
     func getCount<P: Object>(type: P.Type) -> Int {
         guard let database = try? Realm() else { return 0 }
         lck.lock()
